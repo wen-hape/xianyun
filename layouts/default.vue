c0a294ab0492bd07feef74370e400c32fabd5599
mwh <526184195@qq.com>
2019-06-28 09:09:06 +0800

    首页完成

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Header></Header>
+    <nuxt />
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+ul,li{
+  list-style: none;
+}
+a{
+  text-decoration: none;
+  color:inherit;
+}
+em,i{
+  font-style: normal;
+}
+</style>
---
+++ pages/air/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        机票页面
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
